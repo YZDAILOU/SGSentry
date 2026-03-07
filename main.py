e8f768705ff52330f47b5f1d54f386e5b52523cb
Ian Lam <ljy200523@gmail.com>
2026-03-07 17:01:05 +0800

    Include link for front-end

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
 
 # Import our modules
 from agents.transcriber import AudioTranscriber
